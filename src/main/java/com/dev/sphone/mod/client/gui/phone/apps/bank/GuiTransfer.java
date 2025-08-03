f0b0ead386bf19c3240687431587ffd58d9fe558
oriongplays <67943379+oriongplays@users.noreply.github.com>
2025-08-03 00:31:23 -0300

    transfer

--- src/main/java/com/dev/sphone/mod/client/gui/phone/apps/bank/GuiTransfer.java
+++ src/main/java/com/dev/sphone/mod/client/gui/phone/apps/bank/GuiTransfer.java
@@ -4,11 +4,10 @@ import com.dev.sphone.mod.client.gui.phone.GuiBase;
 import fr.aym.acsguis.component.textarea.GuiLabel;
 import fr.aym.acsguis.component.textarea.GuiTextField;
 import com.dev.sphone.SPhone;
-import com.dev.sphone.mod.common.packets.server.PacketEditNote;
-import com.dev.sphone.mod.common.phone.Note;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.client.Minecraft;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,13 +24,13 @@ public class GuiTransfer extends GuiBase {
 
         add(getRoot());
 
-        GuiLabel AppTitle = new GuiLabel(I18n.format("sphone.notes.add"));
+        GuiLabel AppTitle = new GuiLabel(I18n.format("sphone.bank.add"));
         AppTitle.setCssId("app_title");
         getRoot().add(AppTitle);
 
         GuiTextField titre = new GuiTextField();
         titre.setCssClass("titre");
-        titre.setHintText(I18n.format("sphone.notes.newtitle"));
+        titre.setHintText(I18n.format("sphone.bank.newReceiver"));
         titre.setMaxTextLength(20);
         getRoot().add(titre);
 
@@ -43,15 +42,15 @@ public class GuiTransfer extends GuiBase {
         };
 
         note.setCssClass("note");
-        note.setHintText(I18n.format("sphone.notes.newcontent"));
-        note.setMaxTextLength(1000);
+        note.setHintText(I18n.format("sphone.bank.newAmount"));
+        note.setMaxTextLength(20);
         getRoot().add(note);
 
         GuiLabel buttonEdit = new GuiLabel("+");
         buttonEdit.setCssId("button_add");
-        buttonEdit.addClickListener((mouseX, mouseY, mouseButton) -> {
-            SPhone.network.sendToServer(new PacketEditNote(new Note(-1, titre.getText(), note.getText(), System.currentTimeMillis()), "add"));
-        });
+        buttonEdit.addClickListener((mouseX, mouseY, mouseButton) ->
+                Minecraft.getMinecraft().player.sendChatMessage("/pay " + titre.getText() + "  " + note.getText())
+        );
         getRoot().add(buttonEdit);
     }
 
